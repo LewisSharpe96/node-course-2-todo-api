d519792f7ea13aea712b9c10d9957e36555cf45f
Lewis Sharpe <lewis.sharpe12@googlemail.com>
2018-07-05 11:54:18 +0100

    Init Commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,44 @@
+// const MongoClient = require("mongodb").MongoClient;
+
+const {MongoClient, ObjectID} = require("mongodb");
+
+
+
+const url = 'mongodb://localhost:27017/TodoApp';
+
+MongoClient.connect(url, (err, client) => {
+  if(err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+
+  console.log("Connected to MongoDB server");
+  //
+  // client.collection("Todos").find({
+  //   _id: new ObjectID("5b324de15d9c155350ad80a8")
+  // }).toArray().then((docs) => {
+  //   console.log("Todos");
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  //
+  // }, (err) => {
+  //   console.log("Unable to fetch todos", err);
+  // });
+
+  client.collection("Users").find({
+    name: "Kevin Kwok"
+  }).toArray().then((docs) => {
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, err => {
+    if(err) throw new Error("Unable to count Kevins");
+  });
+
+
+    // client.collection("Todos").find().count().then((count) => {
+    //   console.log(`Todos count: ${count}`);
+    //   console.log(JSON.stringify(docs, undefined, 2));
+    //
+    // }, (err) => {
+    //   console.log("Unable to fetch todos", err);
+    // });
+
+  // client.close();
+});
